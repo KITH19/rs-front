22642e1e22dd389ef13cbe9f04974887b9c3cf29
KITH19 <tashutosh1908@gmail.com>
2024-07-27 18:13:07 +0530

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="container">
+        <div className="banner">
+          <div className="left">ZEESHU</div>
+          <div className="right">
+            <p>Udaipur, Rajasthan</p>
+            <p>Open: 08:00 AM - 8:00 PM</p>
+          </div>
+        </div>
+        <div className="banner">
+          <div className="left">
+            <p>Developed By ASHUTOSH TIWARI</p>
+          </div>
+          <div className="right">
+            <p>All Rights Reserved By TIWARI.</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
